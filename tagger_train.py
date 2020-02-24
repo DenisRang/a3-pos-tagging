0ae04d33578b28f379940a35ae5785694056a16a
denisrang <den41998@mail.ru>
2020-02-24 19:05:58 +0300

    code cleaning

--- tagger_train.py
+++ tagger_train.py
@@ -18,30 +18,11 @@ from torch.nn.utils.rnn import pad_sequence, pack_padded_sequence, pad_packed_se
 
 
 class LSTMTagger(nn.Module):
-    EMBEDDING_DIM = 6
-    HIDDEN_DIM = 6
-    K = 3
-
-
     WORD_EMBEDDING_DIM = 10
     CHAR_EMBEDDING_DIM = 6
-    WORD_HIDDEN_DIM = 1024
-    CHAR_HIDDEN_DIM = 1024
-    EPOCHS = 70
-
-    @staticmethod
-    def prepare_sequence(seq, to_ix):
-        idxs = []
-        for w in seq:
-            if w in to_ix:
-                idx = to_ix[w]
-            else:
-                idx = randint(len(to_ix))
-            idxs.append(idx)
-        idxs = torch.tensor(idxs, dtype=torch.long)
-        if USE_CUDA and torch.cuda.is_available():
-            idxs = idxs.cuda()
-        return idxs
+    HIDDEN_DIM = 6
+    K = 3  # CNN sliding window size
+    l = 4  # Number of different convolutional filters
 
     def pad_word(self, word):
         word = list(word)
@@ -51,12 +32,10 @@ class LSTMTagger(nn.Module):
             word.append(UNUSED_ITEM)
         return word
 
-    def __init__(self, word_to_idx, tag_to_idx, char_to_idx, embedding_dim=EMBEDDING_DIM, hidden_dim=HIDDEN_DIM):
+    def __init__(self, word_to_idx, tag_to_idx, char_to_idx):
         super(LSTMTagger, self).__init__()
-        self.word_to_idx = word_to_idx
-        self.tag_to_idx = tag_to_idx
         self.char_to_idx = char_to_idx
-        self.hidden_dim = hidden_dim
+        hidden_dim = self.HIDDEN_DIM
 
         vocab_size = len(word_to_idx)
         tagset_size = len(tag_to_idx)
@@ -64,11 +43,8 @@ class LSTMTagger(nn.Module):
         self.word_embeddings = nn.Embedding(vocab_size, self.WORD_EMBEDDING_DIM)
         self.char_embeddings = nn.Embedding(alphabet_size, self.CHAR_EMBEDDING_DIM)
 
-        in_channel = 1
-        l = 4
-        dw = 1
-        self.lstm = nn.LSTM(self.WORD_EMBEDDING_DIM+l, hidden_dim, bidirectional=True)
-        self.cnn = nn.Conv1d(in_channel, l, self.K * self.CHAR_EMBEDDING_DIM, dw)
+        self.lstm = nn.LSTM(self.WORD_EMBEDDING_DIM + self.l, hidden_dim, bidirectional=True)
+        self.cnn = nn.Conv1d(1, self.l, self.K * self.CHAR_EMBEDDING_DIM, 1)
         # self.drop_out = nn.Dropout(0.2)
 
         # The linear layer that maps from hidden state space to tag space
@@ -83,7 +59,7 @@ class LSTMTagger(nn.Module):
             char_reprs = []
             for word in sentence:
                 word = self.pad_word(word)
-                word = self.prepare_sequence(word, self.char_to_idx)
+                word = CorpusDataset.prepare_sequence(word, self.char_to_idx)
                 word_char_embeds = self.char_embeddings(word)
                 left_right_surrounding = int((self.K - 1) / 2)
                 x_hats = []
@@ -113,16 +89,7 @@ class LSTMTagger(nn.Module):
         return tag_scores
 
 
-# class CNN(nn.Module):
-def prepare_sequence(seq, to_ix):
-    idxs = [to_ix[w] for w in seq]
-    idxs = torch.tensor(idxs, dtype=torch.long)
-    if USE_CUDA and torch.cuda.is_available():
-        idxs = idxs.cuda()
-    return idxs
-
-
-class CustomDataset(Dataset):
+class CorpusDataset(Dataset):
     def __init__(self, training_data, word_to_idx, tag_to_idx):
         self.training_data = training_data
         self.word_to_idx = word_to_idx
@@ -130,28 +97,43 @@ class CustomDataset(Dataset):
 
     def __getitem__(self, index):
         sent = self.training_data[index][0]
-        sent_in = prepare_sequence(sent, self.word_to_idx)
-        tag_in = prepare_sequence(self.training_data[index][1], self.tag_to_idx)
+        sent_in = self.prepare_sequence(sent, self.word_to_idx)
+        tag_in = self.prepare_sequence(self.training_data[index][1], self.tag_to_idx)
         return (sent, sent_in, tag_in)
 
     def __len__(self):
         return len(self.training_data)
 
+    @staticmethod
+    def prepare_sequence(seq, to_ix):
+        idxs = []
+        for w in seq:
+            if w in to_ix:
+                idx = to_ix[w]
+            else:
+                idx = randint(len(to_ix))
+            idxs.append(idx)
+        idxs = torch.tensor(idxs, dtype=torch.long)
+        if USE_CUDA and torch.cuda.is_available():
+            idxs = idxs.cuda()
+        return idxs
+
 
 def pad_collate(batch):
     (sents, sents_in, tags_in) = zip(*batch)
 
-    sents_in_pad = pad_sequence(sents_in, batch_first=True, padding_value=43057)
-    tags_in_pad = pad_sequence(tags_in, batch_first=True, padding_value=134)
+    sents_in_pad = pad_sequence(sents_in, batch_first=True, padding_value=padding_word_idx)
+    tags_in_pad = pad_sequence(tags_in, batch_first=True, padding_value=padding_tag_idx)
 
     return sents, sents_in_pad, tags_in_pad
 
 
-# Use CUDA for training on GPU
-USE_CUDA = False
-NUM_EPOCHS = 1
+USE_CUDA = False  # Use CUDA for training on GPU
 UNUSED_ITEM = '∞'
-BATCH_SIZE = 128
+NUM_EPOCHS = 2
+BATCH_SIZE = 32
+padding_word_idx = 0
+padding_tag_idx = 0
 
 
 def train_model(train_file, model_file):
@@ -191,58 +173,42 @@ def train_model(train_file, model_file):
     word_to_idx[UNUSED_ITEM] = len(word_to_idx)
     tag_to_idx[UNUSED_ITEM] = len(tag_to_idx)
     char_to_idx[UNUSED_ITEM] = len(char_to_idx)
-
+    padding_word_idx = len(word_to_idx) - 1
+    padding_tag_idx = len(tag_to_idx)
 
     model = LSTMTagger(word_to_idx, tag_to_idx, char_to_idx)
     if USE_CUDA and torch.cuda.is_available():
         model = model.cuda()
     loss_function = nn.NLLLoss()
-    optimizer = optim.SGD(model.parameters(), momentum=0.9, lr=10)
+    optimizer = optim.SGD(model.parameters(), lr=3.2)
 
-    total_step = len(training_data)
-    loss_list = []
-    acc_list = []
-    ### Reduce number of epochs, if training data is big
+    train_data = CorpusDataset(training_data, word_to_idx, tag_to_idx)
+    train_loader = DataLoader(dataset=train_data, batch_size=BATCH_SIZE, shuffle=False, collate_fn=pad_collate,
+                              num_workers=BATCH_SIZE)
+
     start_time = time.time()
-
-    train_data = CustomDataset(training_data, word_to_idx, tag_to_idx)
-    train_loader = DataLoader(dataset=train_data, batch_size=BATCH_SIZE, shuffle=False, collate_fn=pad_collate,num_workers=BATCH_SIZE)
-
     for epoch in range(NUM_EPOCHS):  # again, normally you would NOT do 300 epochs, it is toy data
 
         for item in tqdm(train_loader):
-            # Step 1. Remember that Pytorch accumulates gradients.
-            # We need to clear them out before each instance
+            # Clear gradients out before each instance
             model.zero_grad()
             sentence = []
             for i in range(len(item[0])):
                 sent = item[0][i] + [UNUSED_ITEM for i in range(len(item[1][0]) - len(item[0][i]))]
                 sentence.append(sent)
 
-            # Step 3. Run our forward pass.
+            # Forward pass.
             tag_scores = model(sentence, item[1])
-            #
-            # # Step 4. Compute the loss, gradients, and update the parameters by
-            # #  calling optimizer.step()
-            # targets = prepare_sequence(tags, tag_to_idx)
+
             temp_loss = torch.Tensor([0])
             for i in range(len(tag_scores)):
                 temp_loss += loss_function(tag_scores[i], item[2][i])
 
             loss = temp_loss / BATCH_SIZE
-            # print('Loss: ' + str(loss.item()))
-            #         loss_list.append(loss.item())
-            print(loss)
+            print(f'  Loss: {float(loss):.4f}')
             loss.backward()
             optimizer.step()
-            #
-        #         correct = (predicted == targets).sum().item()
-        #         acc_list.append(correct / total)
 
-        #         if (i + 1) % 100 == 0:
-        #             print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}, Accuracy: {:.2f}%'
-        #                   .format(epoch + 1, NUM_EPOCHS, i + 1, total_step, loss.item(),
-        #                           (correct / total) * 100))
         print("Epoch: " + str(epoch))
         print("Loss: " + str(loss))
     print(f'Duration: {time.time() - start_time}')
